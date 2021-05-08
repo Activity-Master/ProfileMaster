64eb7a19fb0621ccea1a162cbbbf0b054c4d9caa
GedMarc <marc.magon@gmail.com>
2021-05-08 18:57:15 +0200

    A bit better on the calls, Imaging Service

--- src/main/java/com/guicedee/activitymaster/profiles/implementations/providers/ProfileServiceDTOProvider.java
+++ src/main/java/com/guicedee/activitymaster/profiles/implementations/providers/ProfileServiceDTOProvider.java
@@ -4,10 +4,11 @@ import com.google.inject.Provider;
 import com.guicedee.activitymaster.profiles.dto.ProfileServiceDTO;
 import com.guicedee.guicedinjection.GuiceContext;
 import com.jwebmp.core.base.ajax.AjaxCall;
-import com.jwebmp.core.utilities.StaticStrings;
 
 import java.util.Map;
 import java.util.UUID;
+
+import static com.jwebmp.core.utilities.StaticStrings.*;
 
 public class ProfileServiceDTOProvider
 		implements Provider<ProfileServiceDTO<?>>
@@ -16,11 +17,11 @@ public class ProfileServiceDTOProvider
 	public ProfileServiceDTO<?> get()
 	{
 		AjaxCall<?> ajaxCall = GuiceContext.get(AjaxCall.class);
-		if(ajaxCall.getVariable(StaticStrings.LOCAL_STORAGE_VARIABLE_KEY) != null)
+		if(ajaxCall.getVariable("localstorage") != null)
 		{
-			Map<String, String> stringStringMap = ajaxCall.getVariable(StaticStrings.LOCAL_STORAGE_VARIABLE_KEY)
+			Map<String, String> stringStringMap = ajaxCall.getVariable("localstorage")
 			                                              .asMap();
-			UUID identityToken = UUID.fromString(stringStringMap.get(StaticStrings.LOCAL_STORAGE_PARAMETER_KEY));
+			UUID identityToken = UUID.fromString(stringStringMap.get(LOCAL_STORAGE_PARAMETER_KEY));
 
 			ProfileServiceDTO<?> pro = new ProfileServiceDTO<>();
 			GuiceContext.inject().injectMembers(pro);
