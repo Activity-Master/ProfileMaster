3fd51beabf57fafc48b2f137412e07deba563dbb
GedMarc <ged_marc@hotmail.com>
2025-07-04 19:59:14 +0200

    Post startup as futures

--- src/main/java/com/guicedee/activitymaster/profiles/implementations/updates/ProfileMasterInstall.java
+++ src/main/java/com/guicedee/activitymaster/profiles/implementations/updates/ProfileMasterInstall.java
@@ -7,6 +7,7 @@ import com.guicedee.activitymaster.fsdm.client.services.systems.ISystemUpdate;
 import com.guicedee.activitymaster.fsdm.client.services.systems.SortedUpdate;
 import com.guicedee.activitymaster.profiles.ProfileSystem;
 import com.guicedee.activitymaster.profiles.enumerations.ProfileIdentificationTypes;
+import io.vertx.core.Future;
 
 import static com.guicedee.activitymaster.profiles.enumerations.ProfileEventTypes.*;
 import static com.guicedee.activitymaster.profiles.enumerations.SiteClientClassifications.*;
@@ -15,36 +16,37 @@ import static com.guicedee.activitymaster.profiles.enumerations.SiteClientClassi
 public class ProfileMasterInstall implements ISystemUpdate
 {
 	@Override
-	public void update(IEnterprise<?,?> enterprise)
+	public Future<Boolean> update(IEnterprise<?,?> enterprise)
 	{
 		createInvolvedPartyClassifications(enterprise);
 		createSiteDetailsClassifications(enterprise);
+		return Future.succeededFuture(true);
 	}
-	
+
 	private void createInvolvedPartyClassifications(IEnterprise<?,?> enterprise)
 	{
-		
+
 		ProfileSystem system = com.guicedee.client.IGuiceContext.get(ProfileSystem.class);
 		ISystems<?,?> profileSystem = system.getSystem(enterprise);
-		
+
 		IEventService<?> eventsService = com.guicedee.client.IGuiceContext.get(IEventService.class);
-	
+
 		eventsService.createEventType(UserRegistered.toString(), profileSystem, system.getSystemToken(enterprise));
 		eventsService.createEventType(VisitorRegistered.toString(), profileSystem, system.getSystemToken(enterprise));
-		
-		
+
+
 		com.guicedee.client.IGuiceContext.get(IInvolvedPartyService.class)
 		            .createIdentificationType(profileSystem, ProfileIdentificationTypes.IdentificationTypeWebClientUUID,
 				            "The Web Client UUID stored as a device identifier",
 				            system.getSystemToken(enterprise));
 	}
-	
+
 	private void createSiteDetailsClassifications(IEnterprise<?,?> enterprise)
 	{
 		IClassificationService<?> classificationService = com.guicedee.client.IGuiceContext.get(IClassificationService.class);
 		ProfileSystem system = com.guicedee.client.IGuiceContext.get(ProfileSystem.class);
 		ISystems<?,?> profileSystem = system.getSystem(enterprise);
-		
+
 		classificationService.create(ClientConnectionDetails, profileSystem);
 		classificationService.create(BrowserDeviceCategory, profileSystem,ClientConnectionDetails);
 		classificationService.create(OperatingSystemFamily, profileSystem, ClientConnectionDetails);
